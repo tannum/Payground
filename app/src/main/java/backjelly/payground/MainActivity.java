f6b05695ba6aaf8e5d9e8bea039584e8fa4c054e
tannum <tannummail@gmail.com>
2016-12-28 07:32:10 +0700

    fsfsf

--- app/src/main/java/backjelly/payground/MainActivity.java
+++ app/src/main/java/backjelly/payground/MainActivity.java
@@ -2,12 +2,47 @@ package backjelly.payground;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scrollView = (ScrollView)findViewById(R.id.scrollView);
+        scrollView.setOnTouchListener(new View.OnTouchListener() {
+
+            int action = event.getAction();
+
+            if(action == MotionEvent.ACTION_DOWN) {
+                startY = event.getY();
+            } else if(action == MotionEvent.ACTION_MOVE) {
+                dist = event.getY() - startY;
+
+                if((pxToDp((int)dist) <= -DISTANCE) && !isMenuHide) {
+                    isMenuHide = true;
+                    hideMenuBar();
+                } else if((pxToDp((int)dist) > DISTANCE) && isMenuHide) {
+                    isMenuHide = false;
+                    showMenuBar();
+                }
+
+                if((isMenuHide && (pxToDp((int)dist) <= -DISTANCE))
+                        || (!isMenuHide && (pxToDp((int)dist) > 0))) {
+                    startY = event.getY();
+                }
+            } else if(action == MotionEvent.ACTION_UP) {
+                startY = 0;
+            }
+
+            return false;
+        });
+
     }
 }
