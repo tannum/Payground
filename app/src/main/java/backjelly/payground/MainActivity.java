20e72011fe134bb645f3c2188212964f0f1b2445
tannum <tannummail@gmail.com>
2016-12-28 17:46:25 +0700

    sfsdff

--- app/src/main/java/backjelly/payground/MainActivity.java
+++ app/src/main/java/backjelly/payground/MainActivity.java
@@ -2,47 +2,68 @@ package backjelly.payground;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
     ScrollView scrollView;
+    RelativeLayout layoutMenu,layoutActionBar, layoutHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getActionBar().hide();
+        layoutMenu = (RelativeLayout)findViewById(R.id.layoutMenu);
+        layoutActionBar = (RelativeLayout)findViewById(R.id.layoutActionBar);
+        layoutHeader = (RelativeLayout)findViewById(R.id.layoutHeader);
+
         scrollView = (ScrollView)findViewById(R.id.scrollView);
         scrollView.setOnTouchListener(new View.OnTouchListener() {
 
-            int action = event.getAction();
+            final int DISTANCE = 3;
+            float startY = 0;
+            float dist = 0;
+            boolean isMenuHide = false;
 
-            if(action == MotionEvent.ACTION_DOWN) {
-                startY = event.getY();
-            } else if(action == MotionEvent.ACTION_MOVE) {
-                dist = event.getY() - startY;
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int action = motionEvent.getAction();
 
-                if((pxToDp((int)dist) <= -DISTANCE) && !isMenuHide) {
-                    isMenuHide = true;
-                    hideMenuBar();
-                } else if((pxToDp((int)dist) > DISTANCE) && isMenuHide) {
-                    isMenuHide = false;
-                    showMenuBar();
+                if(action == MotionEvent.ACTION_DOWN){
+                    startY = motionEvent.getY(); //get initial y value
+                }
+                else if(action == MotionEvent.ACTION_MOVE){
+                    dist = motionEvent.getY() - startY;
+                    if(pxToDp((int)dist))
                 }
 
-                if((isMenuHide && (pxToDp((int)dist) <= -DISTANCE))
-                        || (!isMenuHide && (pxToDp((int)dist) > 0))) {
-                    startY = event.getY();
-                }
-            } else if(action == MotionEvent.ACTION_UP) {
-                startY = 0;
+
+
+
+
+
+
+
+                return false;
             }
-
-            return false;
         });
 
+
+
+
+
+    }
+
+    public int pxToDp(int px) {
+        DisplayMetrics dm = this.getResources().getDisplayMetrics();
+        int dp = Math.round(px / (dm.densityDpi
+                / DisplayMetrics.DENSITY_DEFAULT));
+        return dp;
     }
 }
